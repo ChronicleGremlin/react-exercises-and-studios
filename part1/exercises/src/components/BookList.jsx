4652620ef72e6e76e7f1d82e518c4178db665f00
Alec <alecjost7@gmail.com>
2024-08-21 00:03:46 -0500

    book list

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,30 @@
-export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+import React from 'react';
+import classes from './BookList.module.css';
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
-}
+class BookList extends React.Component {
+   render() {
+      const pageTitle = "Books I Read Every Year";
+      let book1 = "A Court of Thorns and Roses by Sarah J. Maas";
+      let book2 = "Throne of Glass by Sarah J. Maas";
+      let book3 = "City of Bones by Cassandra Clare";
+      let book4 = "Shatter Me by Tahereh Mafi";
+      return (
+         <div>
+            <h3 classname={classes.bookHeading}>{pageTitle}</h3>
+            <li className={classes.bookText}>{book1}</li>
+            <li className={classes.bookText}>{book2}</li>
+            <li className={classes.bookText}>{book3}</li>
+            <li className={classes.bookText}>{book4}</li>
+            <div2>
+               <img src={"https://m.media-amazon.com/images/I/81RrEEMiOCL._AC_UF1000,1000_QL80_.jpg"} alt="A Court of Thorns and Roses by Sarah J. Maas" />
+               <img src={"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1673566495i/76703559.jpg"} alt="Throne of Glass by Sarah J. Maas" />
+               <img src={"https://m.media-amazon.com/images/I/81CXrM8yIqL._AC_UF1000,1000_QL80_.jpg"} alt="City of Bones by Cassandra Clare" />
+               <img src={"https://m.media-amazon.com/images/I/81RxMMXUnFL._AC_UF1000,1000_QL80_.jpg"} alt="Shatter Me by Tahereh Mafi" />
+            </div2>
+         </div >
+      )
+   }
+
+};
+
+export default BookList;
