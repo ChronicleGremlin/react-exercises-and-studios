a0dfb21922682bb72b1302f9ca5e96692351b10c
Alec <alecjost7@gmail.com>
2024-08-20 22:20:18 -0500

    chores list module css

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,16 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+class ChoresList extends React.Component {
+   render() {
+      const chores = ["Make Tea", "Cuddle cats"];
+      return (
+      <div>
+         <h3 className={classes.choresHeading}>Chores to do!</h3>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+      </div>
+      );
+   }
 }
+export default ChoresList;
