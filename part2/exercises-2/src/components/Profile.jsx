3c11cd15f124c033ce98aa0272566266bd6884db
Alec <alecjost7@gmail.com>
2024-08-27 17:53:24 -0500

    part two react exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,29 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+const listItem = oceans.map(oceans =>
+   <div key={oceans.id} className='profile'>
+      <img src={oceans.image} alt={oceans.name} className='img'>
+      </img>
+      <div key={oceans.id} className={`${oceans.fishCheck === 'true' ? 'isAFish' : 'profile'}`}></div>
+      <h1>{oceans.name}</h1>
+      <h2>Fun Ocean Facts:</h2>
+      <ul>
+         <li>{oceans.fact1}</li>
+         <li>{oceans.fact2}</li>
+         <li>{oceans.fact3}</li>
+      </ul>
+      <Button />
+   </div>
+);
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+function Profile() {
+
+   return (
+         <ul>
+            {listItem}
+         </ul>
    );
 }
 
